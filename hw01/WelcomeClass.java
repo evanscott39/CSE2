0543fdd75f8e43d0dc4d296963a7d834e5dd54bc
evanscott39 <egs318@lehigh.edu>
2015-01-26 18:52:59 +0000

    finished WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,25 @@
+//Evan Scott
+//HW01
+//WelcomeClass Homework
+
+//create a class
+public class WelcomeClass{
+    
+    //main method
+    public static void main(String[] args) {
+        
+        //printouts
+        System.out.println("   -----------");
+        System.out.println("   | Welcome |");
+        System.out.println("   -----------");
+        System.out.println("   ^  ^  ^  ^  ^  ^ ");
+        System.out.println("  / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println(" <-E--G--S--3--1--8-> ");
+        System.out.println("  \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("   v  v  v  v  v  v  >" );
+        System.out.println("Over the summer I ate, slept, read, and played soccer.");
+        System.out.println("Not usually in that order");
+    }
+    
+    
+}
